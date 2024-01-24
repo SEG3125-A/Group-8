227e51b32a14b193ddef1fbd4926764502bb850a
vguth104 <vguth104@uottawa.ca>
2024-01-24 14:58:50 -0500

    Added Price display and sorting
    the prices of foods are now displayed when they are selected and in the cart.
    
    The list of foods in the products page and cart are sorted from cheapest to expensive
    
    Added additional Personal info questions including name and organic perference

--- scripts/groceries.js
+++ scripts/groceries.js
@@ -7,66 +7,77 @@ var products = [
 		name: "Brocoli",
 		vegetarian: true,
 		glutenFree: true,
+		organic: true,
 		price: 1.99
 	},
 	{
 		name: "Bread",
 		vegetarian: true,
 		glutenFree: false,
+		organic: false,
 		price: 2.35
 	},
 	{
 		name: "Quinoa",
 		vegetarian: true,
 		glutenFree: true,
+		organic: true,
 		price: 5.35
 	},
 	{
 		name: "Lentille",
 		vegetarian: true,
 		glutenFree: true,
+		organic: true,
 		price: 3.35
 	},
 	{
 		name: "Tofu",
 		vegetarian: true,
 		glutenFree: true,
+		organic: false,
 		price: 8.00
 	},
 	{
 		name: "Salmon",
 		vegetarian: false,
 		glutenFree: true,
+		organic: true,
 		price: 10.00
 	},
 	{
 		name: "Almonds",
 		vegetarian: true,
 		glutenFree: true,
+		organic: false,
 		price: 6.78
 	},
 	{
 		name: "Walnuts",
 		vegetarian: true,
 		glutenFree: true,
+		organic: false,
 		price: 7.98
 	},
 	{
 		name: "Chia Seeds",
 		vegetarian: true,
 		glutenFree: true,
+		organic: false,
 		price: 6.77
 	},
 	{
 		name: "Potato",
 		vegetarian: true,
 		glutenFree: true,
+		organic: true,
 		price: 4.56
 	},
 	{
 		name: "Sweet Potato",
 		vegetarian: true,
 		glutenFree: true,
+		organic: true,
 		price: 5.76
 	},
 
@@ -82,14 +93,14 @@ function restrictListProducts(prods, restriction) {
 	let product_names = [];
 	for (let i=0; i<prods.length; i+=1) {
 		if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
-			product_names.push(prods[i].name);
+			product_names.push(prods[i]);
 		}
 		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
-			product_names.push(prods[i].name);
+			product_names.push(prods[i]);
 		}
 		else if (restriction == "None"){
-			product_names.push(prods[i].name);
-		}
+			product_names.push(prods[i]);
+		}		
 	}
 	return product_names;
 }
@@ -102,5 +113,5 @@ function getTotalPrice(chosenProducts) {
 			totalPrice += products[i].price;
 		}
 	}
-	return totalPrice;
+	return totalPrice.toFixed(2);
 }
--- scripts/main.js
+++ scripts/main.js
@@ -34,32 +34,39 @@ function populateListProductChoices(slct1, slct2) {
 	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
     s2.innerHTML = "";
 		
-	// obtain a reduced list of products based on restrictions
-    var optionArray = restrictListProducts(products, s1.value);
+	// obtain a reduced list of products based on restrictions, is a dictionary so it contains all the fields
+    var optionArray = restrictListProducts(products, s1.value, );
 
+	// sort the list by lowest to highest price
+	optionArray.sort(function(a, b) {
+		return a.price - b.price;
+	});
+	
 	// for each item in the array, create a checkbox element, each containing information such as:
 	// <input type="checkbox" name="product" value="Bread">
-	// <label for="Bread">Bread/label><br>
+	// <label for="Bread">Bread ... $#.##/label><br>
 		
 	for (i = 0; i < optionArray.length; i++) {
 			
-		var productName = optionArray[i];
+		var productName = optionArray[i].name;
+		var productPrice = optionArray[i].price.toFixed(2);
 		// create the checkbox and add in HTML DOM
 		var checkbox = document.createElement("input");
 		checkbox.type = "checkbox";
 		checkbox.name = "product";
-		checkbox.value = productName;
+		checkbox.value = [productName, productPrice];
 		s2.appendChild(checkbox);
 		
 		// create a label for the checkbox, and also add in HTML DOM
 		var label = document.createElement('label')
-		label.htmlFor = productName;
-		label.appendChild(document.createTextNode(productName));
+		label.htmlFor = productName ;
+		label.appendChild(document.createTextNode(productName+" ... $"+productPrice));
 		s2.appendChild(label);
 		
 		// create a breakline node and add in HTML DOM
 		s2.appendChild(document.createElement("br"));    
 	}
+
 }
 	
 // This function is called when the "Add selected items to cart" button in clicked
@@ -80,14 +87,17 @@ function selectedItems(){
 	para.appendChild(document.createElement("br"));
 	for (i = 0; i < ele.length; i++) { 
 		if (ele[i].checked) {
-			para.appendChild(document.createTextNode(ele[i].value));
+			var element = ele[i].value.split(",")
+			var productName = element[0]
+			var productPrice = element[1]
+			para.appendChild(document.createTextNode(productName+" ... $"+productPrice));
 			para.appendChild(document.createElement("br"));
-			chosenProducts.push(ele[i].value);
+			chosenProducts.push(productName);
 		}
 	}
 		
 	// add paragraph and total price
 	c.appendChild(para);
-	c.appendChild(document.createTextNode("Total Price is " + getTotalPrice(chosenProducts)));
+	c.appendChild(document.createTextNode("Total Price is $" + getTotalPrice(chosenProducts)));
 		
 }
